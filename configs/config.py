ee9e1ac939624ff0b2b3361a1eec8aac6833f2af
shuhei1110 <s18s411g@gmail.com>
2024-10-06 02:42:02 +0000

    feat :zap: Add dropout layer, change data size linspace, enable save model.

--- configs/config.py
+++ configs/config.py
@@ -2,11 +2,6 @@ import torch
 
 class BaseConfig():
     def __init__(self):
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-class DataConfig():
-    def __init__(self):
         self.root_dir = '/app/'
         self.train_dir = self.root_dir + 'data/train/'
         self.test_dir = self.root_dir + 'data/test/'
@@ -14,11 +9,18 @@ class DataConfig():
         self.test_submit = self.root_dir + 'data/test_submit.csv'
         self.sample_submit = self.root_dir + 'data/sample_submit.csv'
         self.output_dir = self.root_dir + 'data/output/'
+        self.model_dir = self.root_dir + 'models/'
 
-class ModelConfig():
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class Resnet50dGRU(BaseConfig):
     def __init__(self):
+        super().__init__()
         self.encoder = 'resnet50d'
         self.lr = 1e-4
-        self.epochs = 3
+        self.epochs = 30
         self.batch_size = 4
         self.ch_size = 3
+        self.drop_rate = 0.5
+
+        self.save_dir = self.model_dir + 'resnet50d_gru-001/'
--- train.py
+++ train.py
@@ -27,15 +27,12 @@ torch.cuda.empty_cache()
 logger = logger.Logger().logger
 logger.info('Stert processing...')
 
-base_config = config.BaseConfig()
-data_config = config.DataConfig()
-model_config = config.ModelConfig()
+config = config.Resnet50dGRU()
 
-# df_train_class = pd.read_csv(data_config.train_class)
-df_train_class = pd.read_csv(data_config.train_class).iloc[0:500]
+df_train_class = pd.read_csv(config.train_class)
 
-train_paths = [data_config.train_dir + train_path for train_path in os.listdir(data_config.train_dir)]
-test_paths = [data_config.test_dir + test_path for test_path in os.listdir(data_config.test_dir)]
+train_paths = [config.train_dir + train_path for train_path in os.listdir(config.train_dir)]
+test_paths = [config.test_dir + test_path for test_path in os.listdir(config.test_dir)]
 
 class CustomDataset(Dataset):
     def __init__(self, dataframe, status='train'):
@@ -48,18 +45,18 @@ class CustomDataset(Dataset):
     def __getitem__(self, idx):
         row = self.dataframe.loc[idx]
         if self.status == 'train':
-            video_path = data_config.train_dir + row.loc['file']
+            video_path = config.train_dir + row.loc['file']
         elif self.status == 'valid':
-            video_path = data_config.train_dir + row.loc['file']
+            video_path = config.train_dir + row.loc['file']
         elif self.status == 'test':
-            video_path = data_config.test_dir + row.loc['file']
+            video_path = config.test_dir + row.loc['file']
         else:
             logger.error(f'The status enterd is not one of the expected ones: {self.status}')
             sys.exit()
         images = image_processor.load_image(video_path)
         images = torch.from_numpy(images)
         images = images.permute(0, 3, 2, 1)
-        images = images[torch.linspace(0, images.size(0)-1, 32).long()]
+        images = images[torch.linspace(0, images.size(0)-1, 64).long()]
         images = F.interpolate(images, size=(256, 128), mode='bilinear', align_corners=False)
 
         label = row.loc['class']
@@ -77,14 +74,14 @@ train_dataset = CustomDataset(df_train, status='train')
 valid_dataset = CustomDataset(df_valid, status='valid')
 
 train_dataloader = DataLoader(train_dataset, 
-                              batch_size=model_config.batch_size,
+                              batch_size=config.batch_size,
                               shuffle=True,
                               num_workers=0,
                               pin_memory=True,
                               drop_last=True
                               )
 valid_dataloader = DataLoader(valid_dataset, 
-                              batch_size=model_config.batch_size,
+                              batch_size=config.batch_size,
                               shuffle=True,
                               num_workers=0,
                               pin_memory=True,
@@ -101,15 +98,17 @@ class ClassificationModel(nn.Module):
     def __init__(self):
        super().__init__()
        
-       self.encoder = timm.create_model(model_config.encoder, pretrained=True, in_chans=model_config.ch_size, num_classes=512)
-       self.gru = nn.GRU(512, 128, batch_first=True, bidirectional=False, num_layers=1, dropout=0)
+       self.encoder = timm.create_model(config.encoder, pretrained=True, in_chans=config.ch_size, num_classes=512)
+       self.gru = nn.GRU(512, 128, batch_first=True, bidirectional=False, num_layers=2, dropout=config.drop_rate)
        self.layer = nn.Sequential(
            nn.Linear(128, 64, bias=True),
            nn.BatchNorm1d(64),
            nn.LeakyReLU(0.1),
+           nn.Dropout(config.drop_rate),
            nn.Linear(64, 32, bias=True),
            nn.BatchNorm1d(32),
            nn.LeakyReLU(0.1),
+           nn.Dropout(config.drop_rate),
            nn.Linear(32, 17, bias=True)
            )
        
@@ -117,9 +116,9 @@ class ClassificationModel(nn.Module):
         n_frames = x.shape[1]
         width = x.shape[3]
         height = x.shape[4]
-        x = x.view(model_config.batch_size*n_frames, model_config.ch_size, width, height)
+        x = x.view(config.batch_size*n_frames, config.ch_size, width, height)
         x = self.encoder(x)
-        x = x.view(model_config.batch_size, n_frames, -1)
+        x = x.view(config.batch_size, n_frames, -1)
         x, _ = self.gru(x)
         x = x[:, -1, :]
         x = self.layer(x)
@@ -163,9 +162,9 @@ class MetricsCalculater:
             return roc_auc_score(self.targets, self.probabilities)
 
 # preparation for train
-model = ClassificationModel().to(base_config.device)
+model = ClassificationModel().to(config.device)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=model_config.lr)
+optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
 criterion = nn.CrossEntropyLoss(label_smoothing=0.05)
 
 train_acc = MetricsCalculater('multi')
@@ -173,8 +172,8 @@ valid_acc = MetricsCalculater('multi')
 
 valid_loss_best = float('inf')
 
-for epoch in range(model_config.epochs):
-    logger.info(f'Epoch [{epoch + 1}/{model_config.epochs}]')
+for epoch in range(config.epochs):
+    logger.info(f'Epoch [{epoch + 1}/{config.epochs}]')
 
     #train
     model.train()
@@ -182,8 +181,8 @@ for epoch in range(model_config.epochs):
     train_acc.reset()
 
     for batch_idx, (images, labels) in enumerate(tqdm(train_dataloader)):
-        images = images.to(base_config.device).float()
-        labels = labels.type(torch.LongTensor).to(base_config.device)
+        images = images.to(config.device).float()
+        labels = labels.type(torch.LongTensor).to(config.device)
         
         optimizer.zero_grad()
 
@@ -205,8 +204,8 @@ for epoch in range(model_config.epochs):
     
     with torch.no_grad():
         for batch_idx, (images, labels) in enumerate(tqdm(valid_dataloader)):
-            images = images.to(base_config.device).float()
-            labels = labels.type(torch.LongTensor).to(base_config.device)
+            images = images.to(config.device).float()
+            labels = labels.type(torch.LongTensor).to(config.device)
 
             predicts = model(images)
             loss = criterion(predicts, labels)
@@ -220,6 +219,7 @@ for epoch in range(model_config.epochs):
     if valid_loss  < valid_loss_best:
         valid_loss_best = valid_loss
         logger.info('Validation loss improved')
+        torch.save(model, f'{config.encoder}_gru_001_epoch{epoch}_{valid_acc:.4f}.pth')
         
 
 
